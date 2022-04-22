bd0d75b4a621f3ff253a43f3a87ae41ad53b61f6
alkindi <alkindi@tuta.io>
2022-04-23 01:26:39 +0200

    Add config file, move model to DEVICE

---
+++ src/config.py
@@ -0,0 +1,5 @@
+PATH = '../../data/images_sample'
+CSV_FILE_PATH = '../Dataset_Chest_X_Ray_Sample.csv'
+
+DEVICE = 'cuda'
+BATCH_SIZE = 64
--- src/main.py
+++ src/main.py
@@ -2,16 +2,14 @@ import torch
 import torchvision
 import torchxrayvision as xrv
 
+from config import PATH, CSV_FILE_PATH, DEVICE, BATCH_SIZE
 from dataset import XrayDataset
 from data_loader import get_dataloader
 
-PATH = '../../data/images_sample'
-CSV_FILE_PATH = '../Dataset_Chest_X_Ray_Sample.csv'
-
 dataset = XrayDataset(PATH, CSV_FILE_PATH)
-dataloaders = get_dataloader(dataset, batch_size=1)
+dataloaders = get_dataloader(dataset, batch_size=BATCH_SIZE)
 
-model = xrv.models.DenseNet(weights="densenet121-res224-nih")
+model = xrv.models.DenseNet(weights="densenet121-res224-nih").to(DEVICE)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
@@ -22,6 +20,7 @@ criterion = torch.nn.CrossEntropyLoss()
 def train(model, train_loader, optimizer, criterion, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(DEVICE), target.to(DEVICE)
         optimizer.zero_grad()
         output = model(data)
         loss = criterion(output, target)
@@ -30,7 +29,8 @@ def train(model, train_loader, optimizer, criterion, epoch):
         if batch_idx % 100 == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss.item()))
+                       100. * batch_idx / len(train_loader), loss.cpu().item()))
+
 
 def evaluate(model, val_loader, criterion):
     model.eval()
@@ -38,14 +38,11 @@ def evaluate(model, val_loader, criterion):
     correct = 0
     with torch.no_grad():
         for i, (data, target) in enumerate(val_loader):
-            try:
-                output = model(data)
-            except:
-                print("abc", i, data.shape)
-
-            val_loss += criterion(output, target).item()
+            data, target = data.to(DEVICE), target.to(DEVICE)
+            output = model(data)
+            val_loss += criterion(output, target).cpu().item()
             pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            correct += pred.eq(target.view_as(pred)).sum().cpu().item()
             print("It's working!")
     val_loss /= len(val_loader.dataset)
     print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
