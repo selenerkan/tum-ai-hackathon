2444a494c3bd0d86b55d56d40455adc6ef0d3d30
alkindi <alkindi@tuta.io>
2022-04-22 23:35:15 +0200

    Add eval loop

--- src/pytorch_test.py
+++ src/pytorch_test.py
@@ -24,6 +24,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
 criterion = torch.nn.CrossEntropyLoss()
 
+
 # Training loop for ResNet50
 def train(model, train_loader, optimizer, criterion, epoch):
     model.train()
@@ -38,6 +39,24 @@ def train(model, train_loader, optimizer, criterion, epoch):
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                        100. * batch_idx / len(train_loader), loss.item()))
 
-if __name__== '__main__':
-    for epoch in range(1, 10):
-        train(model, dataloaders['train'], optimizer, criterion, epoch)
+def evaluate(model, val_loader, criterion):
+    model.eval()
+    val_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in val_loader:
+            output = model(data)
+            val_loss += criterion(output, target).item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    val_loss /= len(val_loader.dataset)
+    print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        val_loss, correct, len(val_loader.dataset),
+        100. * correct / len(val_loader.dataset)))
+
+
+if __name__ == '__main__':
+    # for epoch in range(1, 10):
+    #     train(model, dataloaders['train'], optimizer, criterion, epoch)
+
+    evaluate(model, dataloaders['val'], criterion)
